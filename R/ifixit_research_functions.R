f003a7876abb2e9bb9844b8fe69da006adecfd6e
Lisa Oshita <l.oshita@yahoo.com>
2017-08-26 11:51:58 -0700

    added hours unit to time_until_answer

--- R/ifixit_research_functions.R
+++ R/ifixit_research_functions.R
@@ -27,6 +27,7 @@ setup <- function(){
   for (i in empty) {
     x$time_until_answer[i] <- (x$download_date[i] - x$post_date[i])/3600
   }
+  units(x$time_until_answer) <- "Hour"
   #====================================
   # recoding factor variables with more than 10 levels as character variables (title, text, tags...)
   n_levels <- x %>%
